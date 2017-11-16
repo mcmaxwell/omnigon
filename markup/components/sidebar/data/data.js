2e5d72c24b165423f1d5b25ef3c223fe4a26d5ca
Maxim Liutsko <mcmaxwell07@gmail.com>
2017-11-16 13:36:23 +0200

    init

---
+++ markup/components/sidebar/data/data.js
@@ -0,0 +1,18 @@
+sidebar: {
+    voites: [
+        {
+            title: 'Draft',
+            voitesList: [
+                {
+                    image: '',
+                    text: 'New vote',
+                    class: 'active'
+                },
+                {
+                    image: '',
+                    text: 'Serie A Footballer of the Year'
+                }
+            ]
+        }
+    ]
+}
